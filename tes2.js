e35934e09aea14bac9d93bce14d2be3ef9f44b42
Yenyen0325 <wisdomtooth28@gmail.com>
2025-05-02 22:54:58 +0700

    tambah file lagi

---
+++ tes2.js
@@ -0,0 +1,7 @@
+console.log("coba keep tes 2 git hub");
+// git add .  = artinya add semua file 
+// git add nama file = artinya hanya ingin me + 1 file saja 
+// lalu git commit -m "keterangan yang ingin + record kegiatan"
+// git push = di step ini baru masuk ke git hub repo kita 
+
+//  jika ingin hapus git reset 
